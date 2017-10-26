1303b252628ce8590373bc23d41fb41294d9824b
Koshiro Naito <nkc.koshiro.naito@gmail.com>
2017-10-26 10:58:50 +0900

    no message

---
+++ Assets/BulletGenerator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletGenerator : MonoBehaviour {
+    public GameObject BulletobjectPrefab;
+
+    // Use this for initialization
+    void Start () {
+	if (Input.GetMouseButtonDouwn(0))
+        {
+            GameObject igaguri =
+                Instantiate(BulletobjectPrefab) as GameObjct;
+            Bulletobject.GetComponent<Bullet>().shoot(
+                new Vector3(0,200,2000));
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
